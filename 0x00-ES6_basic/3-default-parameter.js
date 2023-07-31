828eb6bb16dfd8d4ed97666430c13c1c590c4c63
MissJuly <joygitau98@gmail.com>
2023-08-01 01:36:08 +0300

    Add 3-default-parameter file

---
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-param-reassign */
+export default function getSumOfHoods(
+  initialNumber,
+  expansion1989,
+  // eslint-disable-next-line comma-dangle
+  expansion2019
+) {
+  return initialNumber + expansion1989 + expansion2019;
+}
