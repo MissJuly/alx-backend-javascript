6b965bbc266a7fe9f27786339541cdb05c2292b0
MissJuly <joygitau98@gmail.com>
2023-08-01 22:53:26 +0300

    Add 0-get_list_students file

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,6 +1,6 @@
 export default function getListStudents() {
   return [
-    { id: 1, firstName: 'Gillaume', location: 'San Fracisco' },
+    { id: 1, firstName: 'Gillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 3, firstName: 'Serena', location: 'San Francisco' },
   ];
