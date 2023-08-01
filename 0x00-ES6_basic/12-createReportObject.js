b654bb40338c0eb09dbcdd7b9eea175675c7e973
MissJuly <joygitau98@gmail.com>
2023-08-01 17:32:53 +0300

    Add 12-createReportObject file

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -2,7 +2,7 @@ export default function createReportObject(employeesList) {
   return {
     allEmployees: employeesList,
 
-    getNumberofDepartments(employeesList) {
+    getNumberOfDepartments(employeesList) {
       return Object.keys(employeesList).length;
     },
   };
