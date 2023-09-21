1ff181e4de589c6fed1aa7ff33c3cc59079aa4f9
MissJuly <joygitau98@gmail.com>
2023-09-22 00:50:28 +0300

    Add task 0

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,19 @@
+/* eslint-disable jest/expect-expect */
+/* eslint-disable jest/prefer-expect-assertions */
+const assert = require('assert');
+
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber', () => {
+  it('floating point whole number', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.0), 3);
+  });
+
+  it("rounding down b's floating point fraction number", () => {
+    assert.strictEqual(calculateNumber(1.0, 2.4), 3);
+  });
+
+  it("rounding a and b's floating point fractional number", () => {
+    assert.strictEqual(calculateNumber(1.4, 2.4), 3);
+  });
+});
