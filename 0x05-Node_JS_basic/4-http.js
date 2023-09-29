a0a32e451505e0181ddc96006ef79d658b409772
MissJuly <joygitau98@gmail.com>
2023-09-29 23:42:23 +0300

    fix errors

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,4 +1,3 @@
-/* eslint-disable jest/require-hook */
 const http = require('http');
 
 const hostname = '127.0.0.1';
--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,4 +1,3 @@
-/* eslint-disable jest/require-hook */
 const http = require('http');
 
 const args = process.argv.slice(2);
